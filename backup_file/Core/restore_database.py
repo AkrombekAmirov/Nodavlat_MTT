c09818fab24d63c2178fa97d02f79285b349f55a
Akrombek <akrombekamirov@gmail.com>
2024-10-24 11:54:23 +0500

    11:54 time 24.10.2024 date

--- backup_file/Core/restore_database.py
+++ backup_file/Core/restore_database.py
@@ -3,19 +3,15 @@ from backup_file.file_path import get_file_path
 
 
 def restore_postgres(backup_file):
-    # Backup faylini tiklash uchun psql buyrug'ini chaqirish
     command = [
         'docker', 'exec', '-i', 'NTMO_kontrakt1',
         'psql', '-U', 'admin_user1', '-d', 'NTMO_database1'
     ]
-
-    # Backup faylni o'qib, psql orqali tiklash
     with open(backup_file, 'r') as f:
         try:
             subprocess.run(command, stdin=f, check=True)
-            print(f"Backup tiklandi: {backup_file}")
         except subprocess.CalledProcessError as e:
-            print(f"Backup tiklashda xatolik: {e.stderr.decode()}")
+            print(f"{e.stderr.decode()}")
 
 
 if __name__ == "__main__":
