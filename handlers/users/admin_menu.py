fe233800907ea1647ac57874e62c45053a72c9c8
Akrombek <akrombekamirov@gmail.com>
2024-11-13 12:26:31 +0500

    12:26 time 13.11.2024 date

--- handlers/users/admin_menu.py
+++ handlers/users/admin_menu.py
@@ -1,17 +1,9 @@
-from file_service.file_read import func_qrcode, process_contract, write_qabul
-from file_service.file_database.file_path import get_file_database_path
-from keyboards.inline.keyboards_inline import choose_visitor
-from keyboards.inline.Dictionary import faculty_file_map1
-from utils.db_api.postgresql1 import file_create_
-from file_service.file_path import get_file_path
+from keyboards.inline.keyboards_inline import choose_admin_1, response_admin
 from utils.db_api.core import DatabaseService
 from aiogram.dispatcher import FSMContext
 from data.config import engine, ADMINS
-from states.button import Form
-from datetime import datetime
 from loader import dp, bot
 from aiogram import types
-from uuid import uuid4
 import logging
 
 logging.basicConfig(filename='bot.log', filemode='w', level=logging.DEBUG,
@@ -20,8 +12,32 @@ logging.basicConfig(filename='bot.log', filemode='w', level=logging.DEBUG,
 db = DatabaseService(engine=engine)
 
 
-@dp.callback_query_handler(lambda call: call.data in ['qabul_yes_admin', 'delete_no_admin'])
+@dp.callback_query_handler(lambda call: call.data in ['delete_no_admin'])
 async def answer(call: types.CallbackQuery, state: FSMContext):
-    if call.data == 'qabul_yes_admin':
         await call.message.delete()
-        await call.message.answer("Qabul qilish uchun ariza qoldiring.", reply_markup=choose_visitor)
+        await call.message.answer(
+            "Foydalanuvchini o'chirish uchun pasport seria va raqamini kiriting!\nNamuna: AA1234567")
+
+@dp.message_handler(types.Message)
+async def adminic(message: types.Message, state: FSMContext):
+    await state.update_data({"passport": message.text})
+    if not db.get_user_by_passport(message.text):
+        await message.answer("Bunday foydalanuvchi topilmadi!", reply_markup=choose_admin_1)
+    elif db.get_user_by_passport(message.text):
+        user = db.get_user_by_passport(message.text)
+        print(user.name, user.faculty, user.contract_number)
+        await message.answer(f"F.I.Sh:{user.name}\n"
+                             f"Yonalish: {user.faculty}\n"
+                             f"Contract number: {user.contract_number}\n"
+                             f"Raqam: {user.telegram_number}\n"
+                             f"Passport: {user.passport}\nRostan o'chirasizmi?", reply_markup=response_admin)
+
+@dp.callback_query_handler(lambda call: call.data in ["yes_admin", "no_admin"])
+async def adminic(call: types.CallbackQuery, state: FSMContext):
+    if call.data == "yes_admin":
+        data = await state.get_data()
+        db.delete_user(data.get("passport"))
+        await call.message.answer("Foydalanuvchi o'chirildi!", reply_markup=choose_admin_1)
+    elif call.data == "no_admin":
+        await call.message.answer("Foydalanuvchi o'chirilmadi!", reply_markup=choose_admin_1)
+    await state.reset_state(with_data=True)
